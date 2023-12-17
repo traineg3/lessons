cfda2ca299229cd93db001e66e293f59f6f87f94
traineg3 <145070583+traineg3@users.noreply.github.com>
2023-12-17 03:51:04 +0200

    Add files via upload

---
+++ lesson_18/js/script.js
@@ -0,0 +1,8 @@
+const icon = document.querySelector('.icon-menu');
+icon.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open');
+});
+const icon2 = document.querySelector('.icon-menu2');
+icon2.addEventListener('click', function () {
+	document.documentElement.classList.toggle('menu-open2');
+});
