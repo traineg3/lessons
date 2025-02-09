80248f740a996cb3915a7c4872fc4944de89380c
traineg3 <145070583+traineg3@users.noreply.github.com>
2025-02-09 13:13:02 +0200

    Add files via upload

---
+++ lesson_47/js/script.js
@@ -0,0 +1,11 @@
+"use strict"
+console.log('Hello world')
+
+let a = 7;
+let b = 7;
+let c;
+c = a +b;
+console.log(c);
+let userName = 'Andrew';
+let inform = `my name ${userName}`;
+console.log(inform)
