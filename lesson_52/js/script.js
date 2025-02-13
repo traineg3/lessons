0cad3d63b02203a60f13e9bb893f6c88368ae9cd
traineg3 <145070583+traineg3@users.noreply.github.com>
2025-02-13 11:45:52 +0200

    Delete lesson_52 directory

--- lesson_52/js/script.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("service-worker.js")
-      .then(() => console.log("Service Worker зареєстровано"))
-      .catch((err) => console.log("Помилка: ", err));
-  }
-
