e7353c5a62a31ede8803e7ea6681050b2ccb9c5e
traineg3 <145070583+traineg3@users.noreply.github.com>
2025-02-23 18:27:38 +0200

    Add files via upload

---
+++ lesson_new4/js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+const icon = document.querySelector('.name-button');
+icon.addEventListener('click', function(){
+    document.documentElement.classList.toggle('menu-open')
+ })
+ const icon1 = document.querySelector('.button-menu');
+icon1.addEventListener('click', function(){
+    document.documentElement.classList.toggle('menu-open')
+ })
+ const allInform = document.querySelector('.all-information');
+allInform.addEventListener('click', function(){
+    document.documentElement.classList.toggle('all-information-open')
+ })
+
+
